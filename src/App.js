6c2104f4309adcdc64cc6ee1f2c0b3474199e6eb
frsiam <siam.frs@gmail.com>
2022-03-16 15:04:14 +0600

    create my own tag and show into UI

--- src/App.js
+++ src/App.js
@@ -4,22 +4,32 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Friend name = 'Ahsan Noyon' proffession = 'Job seeker'></Friend>
     </div>
   );
+}
+
+function Person(){
+  return (
+    <div>
+      <h1>Name: Siam</h1>
+      <p>Proffession: Job seeker</p>
+    </div>
+  )
+}
+
+function Friend(props){
+  console.log(props)
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h3>{props.proffession}</h3>
+    </div>
+  )
 }
 
 export default App;
