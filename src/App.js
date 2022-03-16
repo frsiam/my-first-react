7af8cc3e4c85ce18126226a539ee1b2bbd851765
frsiam <siam.frs@gmail.com>
2022-03-16 16:37:10 +0600

    dynamically create and set

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-const person = {
+const pers = {
   backgroundColor: 'black',
   color: 'white',
   border: '2px solid yellow',
@@ -9,22 +9,39 @@ const person = {
   padding: '20px'
 }
 
+const players = [
+  {name: 'Shakib Al Hasan',job: 'Cricketer', nationality: 'Bangladeshi'},
+  {name: 'Muhammad Yosuf Pathan',job: 'Cricketer', nationality: 'India'},
+  {name: 'Muhammad Hafiz',job: 'Cricketer', nationality: 'Pakistan'},
+  {name: 'Muhammad Salah',job: 'Footballer', nationality: 'Egypt'},
+  {name: 'Irfan',job: 'Boliball', nationality: 'Taiwan'}
+]
+
 function App() {
+  const friends = ['Abdullah al-amin', 'Aminul Haq Nayem','Fahim Shakil','Md Rakibul Hasan Rakib','Ahsan Noyon']
+  const proffessions = ['Job Seeker', 'Bussiness Man', 'Officer']
   return (
     <div className="App">
-      <h1 style={{backgroundColor:"blue",color: 'pink',margin: '20px 300px', padding: '30px', border: '3px dotted pink', borderRadius: '40px'}}>Hello</h1>
+      {/* <h1 style={{backgroundColor:"blue",color: 'pink',margin: '20px 300px', padding: '30px', border: '3px dotted pink', borderRadius: '40px'}}>Hello</h1> */}
+      {
+        players.map(player => <Person name={player}></Person>)
+      }
+      {/* <Perso></Perso>
       <Person></Person>
-      <Person></Person>
-      <Friend name = 'Ahsan Noyon' proffession = 'Job seeker'></Friend>
+      <Person></Person> */}
+      <Friend name = {friends[0]} proffession = {proffessions[1]}></Friend>
+      <Friend name = {friends[1]} proffession = {proffessions[1]}></Friend>
     </div>
   );
 }
 
-function Person(){
+function Person(props){
+  console.log(props)
   return (
-    <div style={person}>
-      <h1>Name: Siam</h1>
-      <p>Proffession: Job seeker</p>
+    <div style={pers}>
+      <h1>{props.name.name}</h1>
+      <h3>{props.name.nationality}</h3>
+      <p>{props.name.job}</p>
     </div>
   )
 }
@@ -33,8 +50,8 @@ function Friend(props){
   console.log(props)
   return (
     <div className='friend'>
-      <h1>Name: {props.name}</h1>
-      <h3>Proffession: {props.proffession}</h3>
+      <h1>{props.name}</h1>
+      <h3>{props.proffession}</h3>
     </div>
   )
 }
